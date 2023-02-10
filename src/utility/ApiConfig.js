72e428efaf6d652ba5322c799aae18aac6764ced
yunusmaestro <yunusmarsetio@gmail.com>
2023-02-10 16:39:20 +0700

    base_url to api

--- src/utility/ApiConfig.js
+++ src/utility/ApiConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // base_url: 'https://api.pasporumkm.com'
-    base_url: 'http://localhost:48690'
+    base_url: 'https://api.pasporumkm.com'
+    // base_url: 'http://localhost:48690'
 }
