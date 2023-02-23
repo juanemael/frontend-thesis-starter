2f1fa455846287859ef56efe0d2ba66e996bc302
yunusmaestro <yunusmarsetio@gmail.com>
2023-02-23 13:38:31 +0700

    Hide Copyright

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -4,17 +4,17 @@ import { Heart } from 'react-feather'
 const Footer = () => {
   return (
     <p className='clearfix mb-0'>
-      <span className='float-md-start d-block d-md-inline-block mt-25'>
-        COPYRIGHT © {new Date().getFullYear()}{' '}
+      {/*<span className='float-md-start d-block d-md-inline-block mt-25'>*/}
+        {/*COPYRIGHT © {new Date().getFullYear()}{' '}*/}
         {/*<a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>*/}
-          Juan Emmanuel Dharmadjaya
+          {/*Juan Emmanuel Dharmadjaya*/}
         {/*</a>*/}
-        <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
-      </span>
-      <span className='float-md-end d-none d-md-block'>
-        Hand-crafted & Made only for UMKM
-        <Heart size={14} />
-      </span>
+        {/*<span className='d-none d-sm-inline-block'>, All rights Reserved</span>*/}
+      {/*</span>*/}
+      {/*<span className='float-md-end d-none d-md-block'>*/}
+        {/*Hand-crafted & Made only for UMKM*/}
+        {/*<Heart size={14} />*/}
+      {/*</span>*/}
     </p>
   )
 }
