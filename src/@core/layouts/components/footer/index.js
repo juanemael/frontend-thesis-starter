e92d953e0c06d09caf00e9f4ba4880d7dc1e7e94
yunusmaestro <yunusmarsetio@gmail.com>
2023-02-23 13:45:23 +0700

    Hide Copyright

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <p className='clearfix mb-0'>
       <span className='float-md-start d-block d-md-inline-block mt-25'>
-        COPYRIGHT © {new Date().getFullYear()}{' '}
+        COPYRIGHT © {new Date().getFullYear()}
         {/*<a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>*/}
           {/*Juan Emmanuel Dharmadjaya*/}
         {/*</a>*/}
